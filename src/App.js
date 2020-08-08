58d807ea7352c01bbf2361b5af160c3e426088e2
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-08-08 17:18:45 +0100

    Set up the inputs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import JSONPretty from 'react-json-pretty';
 
+import { encryptObject, decryptObject } from './cryptoUtils';
+
 function App() {
+  const [key, setKey] = useState('');
+  const [inputStr, setInputStr] = useState('');
+
   return (
     <div className="App">
-      <JSONPretty data={{"name": "Chief Oye"}} />
+      <h3>Enter key</h3>
+      <input value={key} onChange={(e) => setKey(e.target.value)}></input>
+
+      <h3>Input</h3>
+      <textarea
+        value={inputStr}
+        onChange={(e) => setInputStr(e.target.value)}
+      ></textarea>
+
+      <h3>Result</h3>
+      <JSONPretty data={inputStr} />
     </div>
   );
 }
